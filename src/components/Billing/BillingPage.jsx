1c5b2b67ce7625364982f1c669874651936f546b
Carlos Pereira <carloscoto0103@gmail.com>
2022-03-11 22:30:30 -0600

    Add four categories pages: Billing, Inventory, Buys and Reports

---
+++ src/components/Billing/BillingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BillingPage = () => {
+  return (
+    <h1>BillingPage</h1>
+  )
+}
---
+++ src/components/Inventory/InventoryPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const InventoryPage = () => {
+  return (
+    <h1>InventoryPage</h1>
+  )
+}
