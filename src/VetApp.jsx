a15e9ff98e52855350d0351686b0726abb3ab3a2
Carlos Pereira <carloscoto0103@gmail.com>
2022-03-11 22:07:30 -0600

    First commit: Initial state project with sass

---
+++ src/VetApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const VetApp = () => {
+  return (
+    <h1>VetApp</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { VetApp } from './VetApp'
+import './styles/styles.scss';
+
+
+ReactDOM.render(
+    <VetApp />,
+    document.getElementById('root')
+)
