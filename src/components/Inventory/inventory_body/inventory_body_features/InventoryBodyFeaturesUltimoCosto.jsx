2dbface0bc10ffec456e2d2565d42798f3446ea0
Carlos Pereira <carloscoto0103@gmail.com>
2022-03-27 21:19:17 -0600

    Inventory Body Features UC created

--- src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesUltimoCosto.jsx
+++ src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesUltimoCosto.jsx
@@ -2,9 +2,36 @@
 export const InventoryBodyFeaturesUltimoCosto = () => {
 
     return (
-        <div>
-            <p>Ultimo Costo</p>
-            <p>Ultimo Costo</p>
+        <div className="inventory_body-featuresUC">
+            
+            <div className="inventory_body-featuresUC-moneda">
+                <p>Moneda</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-impuesto">
+                <p>Impuesto</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-base">
+                <p>Base</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-fletes">
+                <p>Fletes</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-OCargo">
+                <p>Otro Cargo</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-costo">
+                <p>Costo</p>
+            </div>
+
+            <div className="inventory_body-featuresUC-costoR">
+                <p>Costo Real</p>
+            </div>
+
         </div>
     )
 }
