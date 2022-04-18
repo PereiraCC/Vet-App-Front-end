2ba67161363047b03c4b294264e10a865301ed28
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-18 14:57:12 -0600

    closeCash started

--- src/components/CloseCash/CloseCashPage.jsx
+++ src/components/CloseCash/CloseCashPage.jsx
@@ -1,6 +1,41 @@
 
 export const CloseCashPage = () => {
     return (
-        <h1>CloseCashPage</h1>
+        
+        <div className='closeCash_main'>
+
+            <div className='closeCash_left-box'>
+                
+                <div className='closeCash_left-box-periodo'>
+                    <p>Período</p>
+                </div>
+
+                <div className='closeCash_left-box-apertura'>
+                    <p>Apertura</p>
+                </div>
+
+                <div className='closeCash_left-box-general'>
+                    <p>General</p>
+                </div>
+
+            </div>
+
+            <div className='closeCash_right-box'>
+                
+                <div className='closeCash_right-box-cierre'>
+                    <p>Cierre Caja</p>
+                </div>
+
+                <div className='closeCash_right-box-detalle-tarjetas'>
+                    <p>Detalle tarjetas</p>
+                </div>
+
+                <div className='closeCash_right-box-detalle-operaciones'>
+                    <p>Detalle operaciones</p>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
