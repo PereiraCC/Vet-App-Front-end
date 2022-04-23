13913c15f1af46db2cbf0126a84b7be10f666a26
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-23 13:18:04 -0600

    Customers body carta exoneracion first box added

--- src/components/customers/customersBody/CustomersBodyCartaExoneracion.jsx
+++ src/components/customers/customersBody/CustomersBodyCartaExoneracion.jsx
@@ -1,7 +1,99 @@
-import React from 'react'
+
+import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 export const CustomersBodyCartaExoneracion = () => {
-  return (
-    <h1>Carta Exoneracion</h1>
-  )
+    return (
+        <div className='customers_body-carta-main'>
+            
+            <div className='customers_body-carta-first-box'>
+
+                <div className='customers_body-carta-first-box-cliente'>
+            
+                    <div className='customers_body-carta-first-box-cliente-labelsInputs'>
+                        <p id='lblCECliente'>Cliente</p>
+                        <div className='customers_body-carta-first-box-cliente-labelsInputs-inputs'>
+                            <input type='text' name='CEidCliente' id="txtCEidCliente"/>
+                            <input type='text' name='CENombreCliente' id="txtCENombreCliente"/>
+                        </div>
+                    </div>
+
+                    <div className='customers_body-carta-first-box-cliente-buttons'>
+                        <button id='btnCEBuscarCliente'>Buscar Cliente</button>
+                    </div>
+
+                </div>
+
+                <div className='customers_body-carta-first-box-data'>
+                    
+                    <div className='customers_body-carta-first-box-data-firstLine'>
+                        
+                        <div className='customers_body-carta-first-box-data-firstLine-motivo'>
+                            <p id='lblCEMotivo'>Motivo Exoneracion</p>
+                            <select id='cboCEMotivo'>
+                                <option value='0'>Compras Autorizadas</option>
+                            </select>
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-firstLine-numeroDocumento'>
+                            <p id='lblCEnumeroDocumento'>Numero Documento</p>
+                            <div className='customers_body-carta-first-box-data-firstLine-numeroDocumento-inputs'>
+                                <input type='text' name='CENumeroDocumento' id="txtCENumeroDocumento"/>
+                                <BsFillCheckCircleFill id='btnCECheckDocumento'/>
+                            </div>
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-firstLine-fechaEmision'>
+                            <p id='lblCEfechaEmision'>Fecha Emision</p>
+                            <input type="date" id="txtCEfechaEmision" name="fechaEmision"
+                                min="01-01-2022" max="12-31-2022" />
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-firstLine-fechaVence'>
+                            <p id='lblCEfechaVence'>Fecha Vence</p>
+                            <input type="date" id="txtCEfechaVence" name="fechaVence"
+                                min="01-01-2022" max="12-31-2022" />
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-firstLine-exoneracion'>
+                            <p id='lblCEexoneracion'>Exoneracion</p>
+                            <div className='customers_body-carta-first-box-data-firstLine-exoneracion-inputs'>
+                                <p id='lblCEexoneracionPorcentaje'>%</p>
+                                <input type='text' name='CEExoneracion' id="txtCEExoneracion"/>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='customers_body-carta-first-box-data-secondLine'>
+
+                        <div className='customers_body-carta-first-box-data-secondLine-IV'>
+                            <p id='lblCEIV'>% IV</p>
+                            <input type="text" id="txtCEIV" name="CEIV" />
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-secondLine-descripcion'>
+                            <p id='lblCEDescripcion'>Breve descripcion de para que productos se debe aplicar</p>
+                            <input type="text" id="txtCEDescripcion" name="CEDescripcion" />
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-secondLine-btnAgregarCarta'>
+                            <button id='btnCEAgregarCarta'>Agregar Carta</button>
+                        </div>
+
+                        <div className='customers_body-carta-first-box-data-secondLine-btnEliminarCarta'>
+                            <button id='btnCEEliminarCarta'>Eliminar Carta</button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div className='customers_body-carta-second-box'>
+                <p>Second box</p>
+            </div>
+
+        </div>
+    )
 }
