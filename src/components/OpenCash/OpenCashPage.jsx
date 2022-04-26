855fe8c93260ba97c4d7a0c58c4de9c74dc0afe1
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-26 09:57:11 -0600

    openCash title added

--- src/components/OpenCash/OpenCashPage.jsx
+++ src/components/OpenCash/OpenCashPage.jsx
@@ -1,6 +1,32 @@
 
 export const OpenCashPage = () => {
     return (
-        <h1>Open Cash Page</h1>
+        <div className='openCash_main'>
+
+            <div className='openCash_main-title'>
+                <p id='lblOpenCashTitle'>Registro de Apertura de Caja</p>
+            </div>
+
+            <div className='openCash_main-datosCajero'>
+                <p>Datos del Cajero</p>
+            </div>
+
+            <div className='openCash_main-detalleTotal'>
+                <p>Detalle total</p>
+            </div>
+
+            <div className='openCash_main-detalleDenominacion'>
+                <p>Detalle Denominacion</p>
+            </div>
+
+            <div className='openCash_main-icons'>
+                <p>Icons</p>
+            </div>
+
+            <div className='openCash_main-footer'>
+                <p>Footer</p>
+            </div>
+
+        </div>
     )
 }
