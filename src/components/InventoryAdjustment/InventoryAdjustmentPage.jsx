99f37121c122b1191dd18e71ce8fa096bbe7ea7f
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-26 19:33:30 -0600

    Inventory adjutment header added

--- src/components/InventoryAdjustment/InventoryAdjustmentPage.jsx
+++ src/components/InventoryAdjustment/InventoryAdjustmentPage.jsx
@@ -1,6 +1,24 @@
 
 export const InventoryAdjustmentPage = () => {
     return (
-        <h1>Inventory Adjustment Page</h1>
+        <div className='inventory_adjustment-main'>
+
+            <div className='inventory_adjustment-header'>
+                <p id='lblInventoryAdjTitle'>Ajuste de Inventario</p>
+            </div>
+
+            <div className='inventory_adjustment-datos'>
+                <p>Datos de Inventario</p>
+            </div>
+
+            <div className='inventory_adjustment-table'>
+                <p>Table</p>
+            </div>
+
+            <div className='inventory_adjustment-icons'>
+                <p>Icons</p>
+            </div>
+
+        </div>
     )
 }
