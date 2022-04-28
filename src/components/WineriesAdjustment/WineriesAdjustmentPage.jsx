46e932565efbd5be5004c996244f30d90ccefa5a
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-27 19:18:58 -0600

    Wineries Adjustment header added

--- src/components/WineriesAdjustment/WineriesAdjustmentPage.jsx
+++ src/components/WineriesAdjustment/WineriesAdjustmentPage.jsx
@@ -1,6 +1,34 @@
 
 export const WineriesAdjustmentPage = () => {
     return (
-        <h1>Wineries Adjustment Page</h1>
+        <div className='wineries_adjustment-main'>
+            
+            <div className='wineries_adjustment-header'>
+                
+                <div className='wineries_adjustment-header-input'>
+                    <input type='text' name='numeroAjusteBodega' id='txtNumeroWineriesA'/>
+                </div>
+
+                <p id='lblWineriesATitle'>Ajuste Bodega Inventario</p>
+
+            </div>
+
+            <div className='wineries_adjustment-datos'>
+                <p>datos</p>
+            </div>
+
+            <div className='wineries_adjustment-detalleAjuste'>
+                <p>detalleAjuste</p>
+            </div>
+
+            <div className='wineries_adjustment-table'>
+                <p>table</p>
+            </div>
+
+            <div className='wineries_adjustment-icons'>
+                <p>icons</p>
+            </div>
+
+        </div>
     )
 }
