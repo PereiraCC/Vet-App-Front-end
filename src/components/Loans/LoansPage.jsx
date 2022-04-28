d06f2b5a9298765cb3ca6adf44b278aa50f643c8
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-28 11:44:06 -0600

    Loans header added

--- src/components/Loans/LoansPage.jsx
+++ src/components/Loans/LoansPage.jsx
@@ -1,6 +1,24 @@
 
 export const LoansPage = () => {
     return (
-        <h1>Loans Page</h1>
+        <div className='loans_main'>
+
+            <div className='loans_header'>
+                <p id='lblLoansTitle'>Préstamos de productos</p>
+            </div>
+
+            <div className='loans_datosTraslado'>
+                <p>Datos del traslado</p>
+            </div>
+
+            <div className='loans_datosProductos'>
+                <p>Datos de los productos</p>
+            </div>
+
+            <div className='loans_icons'>
+                <p>icons</p>
+            </div>
+
+        </div>
     )
 }
