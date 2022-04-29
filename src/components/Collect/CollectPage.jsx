7d7a32febdbd4169f1a5c48e143511fc781066fd
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-28 23:35:51 -0600

    Collect header addes

--- src/components/Collect/CollectPage.jsx
+++ src/components/Collect/CollectPage.jsx
@@ -1,6 +1,29 @@
 
 export const CollectPage = () => {
     return (
-        <h1>Collect Page</h1>
+        <div className='collect_main'>
+
+            <div className='collect_main-header'>
+
+                <div className='collect_main-header-input'>
+                    <p id='lblCollectRecibo'>Recibo Nº</p>
+                    <input type='text' id='txtCollectRecibo' name='collectRecibo'/>
+                </div>
+                <p id='lblCollectTitle'>Recibo de Dinero</p>
+            </div>
+
+            <div className='collect_main-body'>
+                <p>Body</p>
+            </div>
+
+            <div className='collect_main-table'>
+                <p>Table</p>
+            </div>
+
+            <div className='collect_main-footer'>
+                <p>Footer</p>
+            </div>
+
+        </div>
     )
 }
