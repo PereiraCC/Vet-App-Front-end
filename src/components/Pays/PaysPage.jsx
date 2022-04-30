effaeaa3e994c887de992e1e2015e58011156ba0
Carlos Pereira <carloscoto0103@gmail.com>
2022-04-30 10:25:22 -0600

    Pays header added

--- src/components/Pays/PaysPage.jsx
+++ src/components/Pays/PaysPage.jsx
@@ -1,6 +1,32 @@
 
 export const PaysPage = () => {
     return (
-        <h1>Pays Page</h1>
+        <div className='pays_main'>
+
+            <div className='pays_main-header'>
+                <p id='lblAbonosPTitle'>Abonos a Proveedores</p>
+            </div>
+
+            <div className='pays_main-datosProveedor'>
+                <p>Datos Proveedor</p>
+            </div>
+
+            <div className='pays_main-body'>
+                <p>Body</p>
+            </div>
+
+            <div className='pays_main-table'>
+                <p>Table</p>
+            </div>
+
+            <div className='pays_main-observaciones'>
+                <p>Observaciones</p>
+            </div>
+
+            <div className='pays_main-icons'>
+                <p>Icons</p>
+            </div>
+
+        </div>
     )
 }
