d3019eb53898ab0d19802ba191641f359cba3319
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-01 15:54:00 -0600

    Collect table refactor

--- src/components/Collect/CollectTable.jsx
+++ src/components/Collect/CollectTable.jsx
@@ -9,21 +9,23 @@ export const CollectTable = () => {
                     <thead>
                         <tr>
                             <th className='collect_table-header-factura'>Factura</th>
+                            <th className='collect_table-header-fecha'>Fecha</th>
                             <th className='collect_table-header-montoFactura'>Monto Factura</th>
                             <th className='collect_table-header-saldoAnt'>Saldo Anterior</th>
+                            <th className='collect_table-header-intereses'>Intereses</th>
                             <th className='collect_table-header-abono'>Abono</th>
                             <th className='collect_table-header-saldoAct'>Saldo Actual</th>
-                            <th className='collect_table-header-cuentaCont'>Cuenta Contable</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td className='collect_table-factura'>test</td>
+                            <td className='collect_table-fecha'>test</td>
                             <td className='collect_table-montoFactura'>test</td>
                             <td className='collect_table-saldoAnt'>test</td>
+                            <td className='collect_table-intereses'>test</td>
                             <td className='collect_table-abono'>test</td>
                             <td className='collect_table-saldoAct'>test</td>
-                            <td className='collect_table-cuentaCont'>test</td>
                         </tr>
                     </tbody>
                 </table>
@@ -31,8 +33,36 @@ export const CollectTable = () => {
             </div>
 
             <div className='collect_table-observaciones'>
-                <p id='lblObservacionesTable'>Observaciones:</p>
-                <input type='text' id='txtObservacionesTable' name='observacionesTable' />
+
+                <div className='collect_table-observaciones-obs'>
+                    <p id='lblObservacionesTable'>Observaciones:</p>
+                    <input type='text' id='txtObservacionesTable' name='observacionesTable' />
+                </div>
+
+                <div className='collect_table-observaciones-cheque'>
+                    
+                    <div className='collect_table-observaciones-cheque-check'>
+                        <div className="collect_table-observaciones-cheque-check-input"> 
+                            <input type="checkbox" id="checkChequeCollect" name="chequeCollect" value="chequeCollect" />
+                        </div>
+                        <label for="checkChequeCollect" id="lblChequeCollect">Cheque</label>
+                    </div>
+
+                    <div className='collect_table-observaciones-cheque-input'>
+                        <input type='text' id='txtChequeCollect' name='chequeCollect' />
+                    </div>
+
+                    <div className='collect_table-observaciones-cheque-cbo'>
+                        <select id='cboChequeCollect' name='cboChequeCollect'>
+                            <option value='1'>Cheque 1</option>
+                            <option value='2'>Cheque 2</option>
+                        </select>
+                    </div>
+                    
+
+                </div>
+
+                
             </div>
 
         </div>
