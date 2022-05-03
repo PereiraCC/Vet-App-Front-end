98a681956b809520a2244223640ab1a70a10813d
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-02 21:16:45 -0600

    Pays Adjustment header added

--- src/components/PaysAdjustment/PaysAdjustmentPage.jsx
+++ src/components/PaysAdjustment/PaysAdjustmentPage.jsx
@@ -1,6 +1,29 @@
 
 export const PaysAdjustmentPage = () => {
     return (
-        <h1>Pays Adjustment Page</h1>
+        <div className='pays_adjustment-main'>
+            
+            <div className='pays_adjustment-header'>
+                <p id='lblAjusteNoPaysA'>Ajuste No.</p>
+                <p id='lblTitlePaysA'>Notas de Crédito y Débito</p>
+            </div>
+
+            <div className='pays_adjustment-datosGenerales'>
+                <p>Datos generales</p>
+            </div>
+
+            <div className='pays_adjustment-body'>
+                <p>Body</p>
+            </div>
+
+            <div className='pays_adjustment-icons'>
+                <p>Icons</p>
+            </div>
+
+            <div className='pays_adjustment-footer'>
+                <p>Footer</p>
+            </div>
+
+        </div>
     )
 }
