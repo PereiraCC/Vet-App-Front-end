a2736ab630c45ba35cfce57b0acda95a6d836e1f
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-04 22:23:55 -0600

    Inventory body features serie added

--- src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesSerie.jsx
+++ src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesSerie.jsx
@@ -1,6 +1,36 @@
 
 export const InventoryBodyFeaturesSerie = () => {
     return (
-        <div>InventoryBodyFeaturesSerie</div>
+        <div className='inventory_body-featuresSerie'>
+
+            <div className='inventory_body-featuresSerie-check'>
+
+                <div className="inventory_body-featuresSerie-check-input"> 
+                    <input type="checkbox" id="checkNSerie" name="NSerie" value="NSerie" />
+                </div>
+                <label for="checkNSerie" id="lblNSerieInventory">Tiene Nº de serie</label>
+
+            </div>
+
+            <div className='inventory_body-featuresSerie-table'>
+                
+                <table className='inventory_body-featuresSerie-table-main'>
+                    <thead>
+                        <tr>
+                            <th className='inventory_body-featuresSerie-table-header-serie'>Serie</th>
+                            <th className='inventory_body-featuresSerie-table-header-year'>Año</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className='inventory_body-featuresSerie-table-serie'>test</td>
+                            <td className='inventory_body-featuresSerie-table-year'>test</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
     )
 }
