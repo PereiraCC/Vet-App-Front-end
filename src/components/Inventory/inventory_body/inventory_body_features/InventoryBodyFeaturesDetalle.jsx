79c85d2c78314970334649b51651e1470b9c3e08
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-04 22:31:47 -0600

    Inventory body features detalle added

--- src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesDetalle.jsx
+++ src/components/Inventory/inventory_body/inventory_body_features/InventoryBodyFeaturesDetalle.jsx
@@ -1,6 +1,25 @@
 
 export const InventoryBodyFeaturesDetalle = () => {
     return (
-        <div>InventoryBodyFeaturesDetalle</div>
+        <div className='inventory_body-featuresDetalle'>
+                
+            <table className='inventory_body-featuresDetalle-table-main'>
+                <thead>
+                    <tr>
+                        <th className='inventory_body-featuresDetalle-table-header-nombre'>Nombre Detalle</th>
+                        <th className='inventory_body-featuresDetalle-table-header-existencia'>Existencias</th>
+                        <th className='inventory_body-featuresDetalle-table-header-moneda'>Moneda</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='inventory_body-featuresDetalle-table-nombre'>test</td>
+                        <td className='inventory_body-featuresDetalle-table-existencia'>test</td>
+                        <td className='inventory_body-featuresDetalle-table-moneda'>test</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
     )
 }
