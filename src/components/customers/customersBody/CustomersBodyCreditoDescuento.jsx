29bb7f1712f79a5d69946bb3b80cf606379c21d5
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-05 22:39:19 -0600

    Customers body credito descuento started

--- src/components/customers/customersBody/CustomersBodyCreditoDescuento.jsx
+++ src/components/customers/customersBody/CustomersBodyCreditoDescuento.jsx
@@ -1,6 +1,33 @@
 
 export const CustomersBodyCreditoDescuento = () => {
-  return (
-    <h1>Credito Descuento</h1>
-  )
+    return (
+        <div className='customers_body-credito-main'>
+
+            <div className='customers_body-credito-btn'>
+                <button id="btnActivarOpeCustomersCre">Activar opciones de Credito</button>
+            </div>
+
+            <div className='customers_body-credito-check'>
+                
+                <div className='customers_body-credito-check-input'> 
+                    <input type='checkbox' id='checkActivarCre' name='activarCre' value='activarCre' />
+                </div>
+                <label for='checkActivarCre' id='lblActivarCre'>Activar Crédito</label>
+
+            </div>
+
+            <div className='customers_body-credito-box'>
+                
+                <div className='customers_body-credito-box-leftSide'>
+                    <p>LeftSide</p>
+                </div>
+
+                <div className='customers_body-credito-box-rightSide'>
+                    <p>rightSide</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
