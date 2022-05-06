5db466fa132f3d79d40c9698216e019af6ed5011
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-05 21:59:12 -0600

    Customers icons updated

--- src/components/customers/CustomersIcons.jsx
+++ src/components/customers/CustomersIcons.jsx
@@ -7,40 +7,52 @@ export const CustomersIcons = () => {
     return (
         <div className='customers_icons-main'>
 
-            <div className="customers_icons-nuevo">
-                <div className="customers_icons-nuevo-icon">
-                    <HiDocumentAdd id='iconNuevoC'/>
+            <div className='customers_icons-icon'>
+                <div className="customers_icons-nuevo">
+                    <div className="customers_icons-nuevo-icon">
+                        <HiDocumentAdd id='iconNuevoC'/>
+                    </div>
+                    <p id='lblNuevoC'>Nuevo</p>
                 </div>
-                <p id='lblNuevoC'>Nuevo</p>
+
+                <div className='customers_icons-Buscar'>
+                    <div className="customers_icons-Buscar-icon">
+                        <FaSearch id='iconBusC'/>
+                    </div>
+                    <p id='lblBuscarC'>Buscar</p>
+                </div>
+
+                <div className="customers_icons-registrar">
+                    <div className="customers_icons-registrar-icon">
+                        <FaRegSave id='iconRegistrarC'/>
+                    </div>
+                    <p id='lblRegistrarC'>Registrar</p>
+                </div>
+
+                <div className="customers_icons-eliminar">
+                    <div className="customers_icons-eliminar-icon">
+                        <RiDeleteBin2Fill id='iconEliminarC'/>
+                    </div>
+                    <p id='lblEliminarC'>Eliminar</p>
+                </div>
+
+                <div className="customers_icons-cerrar">
+                    <div className="customers_icons-cerrar-icon">
+                        <ImCancelCircle id='iconCerrarC'/>
+                    </div>
+                    <p id='lblCerrarC'>Cerrar</p>
+                </div>
             </div>
 
-            <div className='customers_icons-Buscar'>
-                <div className="customers_icons-Buscar-icon">
-                    <FaSearch id='iconBusC'/>
-                </div>
-                <p id='lblBuscarC'>Buscar</p>
+            <div className='customers_icons-user'>
+                
+                <p id='lblUserCustomers'>Usuario →</p>
+                <input id='txtUserCustomers' name='userCustomers' type='text' />
+                <p id='lblSistemaCustomers'>SISTEMA</p>
+
             </div>
 
-            <div className="customers_icons-registrar">
-                <div className="customers_icons-registrar-icon">
-                    <FaRegSave id='iconRegistrarC'/>
-                </div>
-                <p id='lblRegistrarC'>Registrar</p>
-            </div>
-
-            <div className="customers_icons-eliminar">
-                <div className="customers_icons-eliminar-icon">
-                    <RiDeleteBin2Fill id='iconEliminarC'/>
-                </div>
-                <p id='lblEliminarC'>Eliminar</p>
-            </div>
-
-            <div className="customers_icons-cerrar">
-                <div className="customers_icons-cerrar-icon">
-                    <ImCancelCircle id='iconCerrarC'/>
-                </div>
-                <p id='lblCerrarC'>Cerrar</p>
-            </div>
+            
 
         </div>
     )
