e05fbc57176c357713ff7ed58051d9f04fa166e4
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-19 16:35:40 -0600

    Category Actions added

--- src/components/CategoryActions/CategoryActions.jsx
+++ src/components/CategoryActions/CategoryActions.jsx
@@ -1,6 +1,67 @@
+import { HiDocumentAdd } from 'react-icons/hi';
+import { FaRegSave, FaSearch } from 'react-icons/fa';
+import { RiDeleteBin2Fill } from 'react-icons/ri';
+import { AiFillEdit } from 'react-icons/ai';
 
 export const CategoryActions = () => {
     return (
-        <h1>CategoryActions</h1>
+        <div className='category_actions-main'>
+
+            <div className='category_actions-header'>
+                <p id='lblTitlePresentation'>Categoria de Acción</p>
+            </div>
+
+            <div className='category_actions-body'>
+                
+                <div className='category_actions-body-tipo-label'>
+                    <p id="lblTipoPresentation">Nombre</p>
+                </div>
+
+                <div className='category_actions-body-tipo-input'>
+                    <input type="text" id="txtTipoPresentation" name='nombreCategoryAction' />
+                </div>
+
+            </div>
+
+            <div className='category_actions-icons'>
+                
+                <div className='category_actions-icons-nuevo'>
+                    <div className='category_actions-icons-nuevo-icon'>
+                        <HiDocumentAdd id='iconNuevoCategoryAction'/>
+                    </div>
+                    <p id='lblNuevoCategoryAction'>Nuevo</p>
+                </div>
+
+                <div className='category_actions-icons-registrar'>
+                    <div className='category_actions-icons-registrar-icon'>
+                        <FaRegSave id='iconRegistrarCategoryAction'/>
+                    </div>
+                    <p id='lblRegistrarCategoryAction'>Guardar</p>
+                </div>
+
+                <div className='denominationCoins-icons-Editar'>
+                    <div className='denominationCoins-icons-Editar-icon'>
+                        <AiFillEdit id='iconEditarCategoryAction'/>
+                    </div>
+                    <p id='lblEditarCategoryAction'>Editar</p>
+                </div>
+
+                <div className='category_actions-icons-Buscar'>
+                    <div className='category_actions-icons-Buscar-icon'>
+                        <FaSearch id='iconBuscarCategoryAction'/>
+                    </div>
+                    <p id='lblBuscarCategoryAction'>Buscar</p>
+                </div>
+
+                <div className='category_actions-icons-eliminar'>
+                    <div className='category_actions-icons-eliminar-icon'>
+                        <RiDeleteBin2Fill id='iconEliminarCategoryAction'/>
+                    </div>
+                    <p id='lblEliminarCategoryAction'>Eliminar</p>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
