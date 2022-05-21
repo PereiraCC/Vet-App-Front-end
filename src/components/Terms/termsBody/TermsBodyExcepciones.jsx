b7f835aa5e8e62a705393ad570cd62910ab35ee6
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-21 16:26:02 -0600

    Terms body excepciones added

--- src/components/Terms/termsBody/TermsBodyExcepciones.jsx
+++ src/components/Terms/termsBody/TermsBodyExcepciones.jsx
@@ -1,6 +1,56 @@
 
 export const TermsBodyExcepciones = () => {
     return (
-        <h1>TermsBodyExcepciones</h1>
+        <div className='terms_body-excepciones-main'>
+            
+            <div className='terms_body-excepciones-title'>
+                <h2 id='lblTitleTermsCondicciones'>Excepciones</h2>
+            </div>
+
+            <div className='terms_body-excepciones-body'>
+                
+                <div className='terms_body-excepciones-body-inputs'>
+
+                    <div className='terms_body-excepciones-body-inputs-cedula'>
+                        <p id='lblCedulaExcepcionesTerms'>Cédula</p>
+                        <input type='text' id='txtCedulaExcepcionesTerms' name='cedulaExcepcionesTerms' />
+                    </div>
+
+                    <div className='terms_body-excepciones-body-inputs-nombre'>
+                        <p id='lblNombreExcepcionesTerms'>Nombre</p>
+                        <input type='text' id='txtNombreExcepcionesTerms' name='nombreExcepcionesTerms' />
+                    </div>
+
+                </div>
+
+                <div className='terms_body-excepciones-body-btns'>
+                    <button id='btnAgregarExcepcionesTerms'>Agregar</button>
+                    <button id='btnEliminarExcepcionesTerms'>Eliminar</button>
+                </div>
+
+            </div>
+
+            <div className='terms_body-excepciones-table'>
+                <table className='terms_body-excepciones-table-main'>
+                        <thead>
+                            <tr>
+                                <th className='terms_body-excepciones-table-header-cedula'>Cedula</th>
+                                <th className='terms_body-excepciones-table-header-nombre'>Nombre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className='terms_body-excepciones-table-cedula'>test</td>
+                                <td className='terms_body-excepciones-table-nombre'>test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+            </div>
+
+            <div className='terms_body-excepciones-btn'>
+                <button id='btnAceptarTermsExcepciones'>Aceptar</button>
+            </div>
+
+        </div>
     )
 }
