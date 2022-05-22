aab0dafb3a3082ab9bc7ca5f9ed53754ddb09384
Carlos Pereira <carloscoto0103@gmail.com>
2022-05-22 12:06:03 -0600

    Proforma header added

--- src/components/Proforma/ProformaPage.jsx
+++ src/components/Proforma/ProformaPage.jsx
@@ -1,6 +1,34 @@
 
 export const ProformaPage = () => {
     return (
-        <h1>ProformaPage</h1>
+        <div className='proforma-main'>
+
+            <div className='proforma-header'>
+                
+                <div className='proforma-header-input'>
+                    <p id='lblProformaCotizacion'>Cotización Nº</p>
+                    <input type='text' id='txtProformaCotizacion' name='proformaCotizacion'/>
+                </div>
+                <p id='lblProformaTitle'>Formula de Cotizaciones</p>
+
+            </div>
+
+            <div className='proforma-datos'>
+                <p>datos</p>
+            </div>
+
+            <div className='proforma-table'>
+                <p>table</p>
+            </div>
+
+            <div className='proforma-totales'>
+                <p>totales</p>
+            </div>
+
+            <div className='proforma-icons'>
+                <p>icons</p>
+            </div>
+
+        </div>
     )
 }
