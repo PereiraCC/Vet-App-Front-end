8d0ab72d386a0d2f4b7e9c1184d8043f6b816f3a
Carlos Pereira <carloscoto0103@gmail.com>
2022-06-04 09:12:30 -0600

    Purchase returns datos factura added

--- src/components/PurchaseReturns/PurchaseReturnsDatosFactura.jsx
+++ src/components/PurchaseReturns/PurchaseReturnsDatosFactura.jsx
@@ -50,7 +50,83 @@ export const PurchaseReturnsDatosFactura = () => {
             </div>
 
             <div className='purchaseReturn_datosFactura-secondLine'>
-                <p>SecondLine</p>
+
+                <div className='purchaseReturn_datosFactura-secondLine-table'>
+                    <div className='purchaseReturn_datosFactura-secondLine-table-title'>
+                        <p id='lblTitleTableDatosFacturaPR'>Datos de los Artículos Facturados</p>
+                    </div>
+
+                    <div className='purchaseReturn_datosFactura-secondLine-table-body'>
+                        <table className='purchaseReturn_datosFactura-secondLine-table-body-main'>
+                            <thead>
+                                <tr>
+                                    <th className='purchaseReturn_datosFactura-secondLine-table-body-header-codigo'>Código</th>
+                                    <th className='purchaseReturn_datosFactura-secondLine-table-body-header-descrip'>Descripción</th>
+                                    <th className='purchaseReturn_datosFactura-secondLine-table-body-header-lote'>Lote</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className='purchaseReturn_datosFactura-secondLine-table-body-codigo'>test</td>
+                                    <td className='purchaseReturn_datosFactura-secondLine-table-body-descrip'>test</td>
+                                    <td className='purchaseReturn_datosFactura-secondLine-table-body-lote'>test</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className='purchaseReturn_datosFactura-secondLine-inputs'>
+
+                    <div className='purchaseReturn_datosFactura-secondLine-inputs-title'></div>
+
+                    <div className='purchaseReturn_datosFactura-secondLine-inputs-input'>
+                        <div className='purchaseReturn_datosFactura-secondLine-inputs-input-left'>
+                            
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-left-codigo'>
+                                <p id='lblCodigoArticuloPR'>Código</p>
+                                <input id='txtCodigoArticuloPR' type='text' name='codigoArticuloPR' />
+                            </div>
+
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-left-precioCosto'>
+                                <p id='lblPrecioCostoPR'>Precio Costo</p>
+                                <input id='txtPrecioCostoPR' type='text' name='precioCostoArticuloPR' />
+                            </div>
+
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-left-descuento'>
+                                <p id='lblDescuentoArticuloPR'>Descuento</p>
+                                <input id='txtDescuentoArticuloPR' type='text' name='descuentoArticuloPR' />
+                            </div>
+
+                        </div>
+
+                        <div className='purchaseReturn_datosFactura-secondLine-inputs-input-right'>
+                            
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-right-lote'>
+                                <p id='lblLoteArticuloPR'>Lote</p>
+                                <input id='txtLoteArticuloPR' type='text' name='loteArticuloPR' />
+                            </div>
+
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-right-cantOriginal'>
+                                <p id='lblCantidadOriginalPR'>Cant Original</p>
+                                <input id='txtCantidadOriginalPR' type='text' name='cantidadOriginalArticuloPR' />
+                            </div>
+
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-right-devoluciones'>
+                                <p id='lblDevolucionesPR'>Devoluciones</p>
+                                <input id='txtDevolucionesPR' type='text' name='devolucionesArticuloPR' />
+                            </div>
+
+                            <div className='purchaseReturn_datosFactura-secondLine-inputs-input-right-devolucion'>
+                                <p id='lblDevolucionPR'>Devolución</p>
+                                <input id='txtDevolucionPR' type='hidden' name='devolucionArticuloPR' />
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
+
             </div>
 
         </div>
