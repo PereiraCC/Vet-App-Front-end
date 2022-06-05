d7391c5eb550ed8ec685fe4528dc5b0387bdf9f2
Carlos Pereira <carloscoto0103@gmail.com>
2022-06-05 08:26:56 -0600

    Pretake body added

--- src/components/Pretake/PretakeBody.jsx
+++ src/components/Pretake/PretakeBody.jsx
@@ -20,7 +20,56 @@ export const PretakeBody = () => {
             </div>
 
             <div className='pretake_body-datosProductos'>
-                <p>datosProductos</p>
+
+                <div className='pretake_body-datosProductos-title'>
+                    <p id='lblTitleDatosProductos'>Datos de los productos</p>
+                </div>
+
+                <div className='pretake_body-datosProductos-inputs'>
+
+                    <div className='pretake_body-datosProductos-inputs-codigo'>
+                        <p id='lblCodigoPretake'>Código</p>
+                        <input type='text' id='txtCodigoPretake' name='codigoPretake' />
+                    </div>
+
+                    <div className='pretake_body-datosProductos-inputs-descripcion'>
+                        <p id='lblDescripcionPretake'>Descripción</p>
+                        <input type='text' id='txtDescripcionPretake' name='descripcionPretake' />
+                    </div>
+
+                    <div className='pretake_body-datosProductos-inputs-existencia'>
+                        <p id='lblExistenciaPretake'>Existencia actual</p>
+                        <input type='text' id='txtExistenciaPretake' name='existenciaPretake' />
+                    </div>
+
+                    <div className='pretake_body-datosProductos-inputs-cantidad'>
+                        <p id='lblcantidadPretake'>Cantidad para prestamo</p>
+                        <input type='text' id='txtCantidadPretake' name='cantidadPretake' />
+                    </div>
+
+                    <div className='pretake_body-datosProductos-inputs-btn'>
+                        <button id='btnAgregarPretake'>Agregar</button>
+                    </div>
+                </div>
+
+                <div className='pretake_body-datosProductos-table'>
+                    <table className='pretake_body-datosProductos-table-main'>
+                        <thead>
+                            <tr>
+                                <th className='pretake_body-datosProductos-table-header-codigo'>Código</th>
+                                <th className='pretake_body-datosProductos-table-header-descrip'>Descripción</th>
+                                <th className='pretake_body-datosProductos-table-header-cantidad'>Cantidad</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className='pretake_body-datosProductos-table-codigo'>test</td>
+                                <td className='pretake_body-datosProductos-table-descrip'>test</td>
+                                <td className='pretake_body-datosProductos-table-cantidad'>test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
