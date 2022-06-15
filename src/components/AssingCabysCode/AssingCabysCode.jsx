841eabad240c258bd27e22125c5ad11c835a5d19
Carlos Pereira <carloscoto0103@gmail.com>
2022-06-14 21:15:39 -0600

    Assign Cabys Code added

--- src/components/AssingCabysCode/AssingCabysCode.jsx
+++ src/components/AssingCabysCode/AssingCabysCode.jsx
@@ -1,6 +1,37 @@
+import { FaSearch } from 'react-icons/fa';
+import { ImCancelCircle} from 'react-icons/im';
+import { AssingCabysCodeBody } from './AssingCabysCodeBody';
 
 export const AssingCabysCode = () => {
     return (
-        <h1>AssingCabysCode</h1>
+        <div className='assingCabysCode-main'>
+
+            <div className='assingCabysCode-header'>
+                <p id='lblTitleAssingCabysCode'>Asignar Codigo Cabys</p>
+            </div>
+
+            <div className='assingCabysCode-body'>
+                <AssingCabysCodeBody />
+            </div>
+
+            <div className='assingCabysCode-icons'>
+                
+                <div className='assingCabysCode-icons-Buscar'>
+                    <div className='assingCabysCode-icons-Buscar-icon'>
+                        <FaSearch id='iconBuscarAssingCabysCode'/>
+                    </div>
+                    <p id='lblBuscarAssingCabysCode'>Buscar</p>
+                </div>
+
+                <div className='assingCabysCode-icons-cerrar'>
+                    <div className='assingCabysCode-icons-cerrar-icon'>
+                        <ImCancelCircle id='iconCerrarAssingCabysCode'/>
+                    </div>
+                    <p id='lblCerrarAssingCabysCode'>Cerrar</p>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
---
+++ src/components/AssingCabysCode/AssingCabysCodeBody.jsx
@@ -0,0 +1,87 @@
+
+export const AssingCabysCodeBody = () => {
+    return (
+        <div className='assingCabysCode_body-main'>
+
+            <div className='assingCabysCode_body-inputs'>
+
+                <div className='assingCabysCode_body-inputs-firstBox'>
+
+                    <div className='assingCabysCode_body-inputs-firstBox-count'>
+                        <p id='lblAssignCabysCodeCount'>992.00</p>
+                    </div>
+
+                    <div className='assingCabysCode_body-inputs-firstBox-opcion'>
+                        <p id='lblAssingCabysCodeOpcion'>Opcion para mostrar articulos:</p>
+                        <select id='cboAssignCabysCodeOpcion' name='assignCabysCodeOpcion'>
+                            <option value='1'>Mostrar solo artículos sin código cabys</option>
+                            <option value='2'>Mostrar solo artículos con código cabys</option>
+                            <option value='3'>Mostrar todos los artículos</option>
+                        </select>
+                    </div>
+                
+                </div>
+
+                <div className='assingCabysCode_body-inputs-secondBox'>
+                    
+                    <div className='assingCabysCode_body-inputs-secondBox-check'>
+                        <div className="assingCabysCode_body-inputs-secondBox-check-input">
+                            <input type="checkbox" id="checkActivosAssingCabysCode" name="ActivosAssingCabysCode" value="ActivosAssingCabysCode" />
+                        </div>
+                        <label for="checkActivosAssingCabysCode" id="lblActivosAssingCabysCode">Mostar solo Activos</label>
+                    </div>
+
+                    <div className='assingCabysCode_body-inputs-secondBox-buscar'>
+                        <p id='lblBuscarDescripcionAssingCabysCode'>Buscar Articulos por Descripcion:</p>
+                        <input type='text' id='txtBuscarDescripcionAssingCabysCode' name='buscarDescripcionAssingCabysCode' />
+                    </div>
+
+                </div>
+
+                <div className='assingCabysCode_body-inputs-thirdBox'>
+
+                    <div className='assingCabysCode_body-inputs-thirdBox-firstLine'>
+                        <button id='btnProductoAgriculaACC'>Producto Agricula (MAG)</button>
+                        <button id='btnQuitarMAGACC'>Quitar MAG</button>
+                    </div>
+
+                    <div className='assingCabysCode_body-inputs-thirdBox-secondLine'>
+                        <button id='btnAsignarCabysACC'>Asignar Cabys</button>
+                        <button id='btnPuntosACC'>...</button>
+                        <button id='btnQuitarCodigoACC'>Quitar Codigo Cabys</button>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='assingCabysCode_body-table'>
+                <table className='assingCabysCode_body-table-main'>
+                    <thead>
+                        <tr>
+                            <th className='assingCabysCode_body-table-header-codArticulo'>Cod_Artículo</th>
+                            <th className='assingCabysCode_body-table-header-descrip'>Descripción</th>
+                            <th className='assingCabysCode_body-table-header-proveedor'>Proveedor</th>
+                            <th className='assingCabysCode_body-table-header-existencia'>Existencia</th>
+                            <th className='assingCabysCode_body-table-header-mag'>MAG</th>
+                            <th className='assingCabysCode_body-table-header-servicio'>Servicio</th>
+                            <th className='assingCabysCode_body-table-header-codcabys'>CodCabys</th>
+                            <th className='assingCabysCode_body-table-header-cabys'>Cabys</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className='assingCabysCode_body-table-codArticulo'>test</td>
+                            <td className='assingCabysCode_body-table-descrip'>test</td>
+                            <td className='assingCabysCode_body-table-proveedor'>test</td>
+                            <td className='assingCabysCode_body-table-existencia'>test</td>
+                            <td className='assingCabysCode_body-table-mag'>test</td>
+                            <td className='assingCabysCode_body-table-servicio'>test</td>
+                            <td className='assingCabysCode_body-table-codcabys'>test</td>
+                            <td className='assingCabysCode_body-table-cabys'>test</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
